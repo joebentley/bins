92f43779d1a620b4ca7f617ea178516d3b213819
Joe D. Bentley <joebentley10@gmail.com>
2014-07-18 15:22:14 +0100

    Email test

--- web.js
+++ web.js
@@ -1,4 +1,16 @@
 
+var sendgrid = require("sendgrid")(process.env.SENDGRID_USERNAME, process.env.SENDGRID_PASSWORD);
+
+sendgrid.send({
+  to:       'joebentley10@gmail.com',
+  from:     'other@example.com',
+  subject:  'Hello World',
+  text:     'My first email through SendGrid.'
+}, function(err, json) {
+  if (err) { return console.error(err); }
+  console.log(json);
+});
+
 var express = require("express");
 var logfmt = require("logfmt");
 var app = express();
