e64e6825383c9be4d01bef24baf45c7f3b63edc1
Joe D. Bentley <joebentley10@gmail.com>
2014-07-19 12:13:13 +0100

    Handled POST, added mongoose

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,10 @@
  * GET home page.
  */
 
-exports.index = function(req, res){
+exports.index = function(req, res) {
   res.render('index', { title: 'Bin Collection' });
+};
+
+exports.signup = function(req, res) {
+  res.render('signup', { email: req.body.email, day: req.body.day });
 };
